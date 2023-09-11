435c410f3691c635efdd962edede404f1834df25
zanko-o <oghabpersianbax@gmail.com>
2023-09-11 19:35:10 +0430

    add-files

---
+++ E6/first-app/src/App.js
@@ -0,0 +1,11 @@
+import Footer from './Footer';
+function App() {
+  return (
+    <div className="App">
+      <h1>sabzlearn cousre react E61</h1>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ E6/first-app/src/Footer.js
@@ -0,0 +1,9 @@
+
+function Footer() {
+    return (
+        <div className="Footer">
+            <h1>i am foorer</h1>
+        </div>
+    );
+}
+export default Footer
