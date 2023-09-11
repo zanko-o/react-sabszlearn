53a44f742bf335abc6be63fde0c7a3810e60b998
zanko-o <oghabpersianbax@gmail.com>
2023-09-11 19:36:38 +0430

    add

--- E6/first-app/src/App.js
+++ E6/first-app/src/App.js
@@ -2,7 +2,7 @@ import Footer from './Footer';
 function App() {
   return (
     <div className="App">
-      <h1>sabzlearn cousre react E61</h1>
+      <h1>sabzlearn cousre react E6</h1>
       <Footer></Footer>
     </div>
   );
